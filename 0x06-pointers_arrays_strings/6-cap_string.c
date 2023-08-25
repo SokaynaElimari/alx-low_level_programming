5c55ecbf8e2f30c011b7bb9a082ea01bdf09af44
SokaynaElimari <elimarisokayna@gmail.com>
2023-08-25 12:46:30 -0700

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,9 @@
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
+{
 int index = 0;
+{
 while (str[index])
 while (!((str[index] >= 'a' && (str[index] <= 'z'))
 index++;
